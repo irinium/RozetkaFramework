65583c2dd6bf7d18ea3238d64b5f5d03437aa414
irinium <iryna.shvets999@gmail.com>
2020-03-29 00:07:25 +0200

    Generic PageObject created

---
+++ src/main/java/ua/com/rozetka/pages/CheckoutPage.java
@@ -0,0 +1,39 @@
+package ua.com.rozetka.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.testng.Assert.assertTrue;
+
+public class CheckoutPage extends GenericPage {
+    
+    @FindBy(css = "#total_block .check-edit-order-link")
+    private WebElement editOrder;
+
+    @FindBy(css = ".cart-check-wrap")
+    private WebElement deleteItem;
+
+    @FindBy(css = ".cart-i-delete a")
+    private WebElement confirmDelete;
+
+    @FindBy(css = ".preloader-big")
+    private WebElement loader;
+
+    @FindBy(css = ".cart-modal__inner .cart-modal__dummy")
+    private WebElement cardStatus;
+
+    public CheckoutPage(WebDriver driver) {
+        super(driver);
+        this.PAGE_TITLE = "ROZETKA— Авторизованный пользователь | Оформление заказа";
+        this.PAGE_URL = "https://my.rozetka.com.ua/checkout/#step=delivery";
+    }
+
+    public void deleteItemFromCard(String item){
+        waitForElementDisappear(loader);
+        clickElement(editOrder);
+        clickElement(deleteItem);
+        clickElement(confirmDelete);
+        assertTrue(getText(cardStatus).trim().contains(item));
+    }
+}
--- src/main/java/ua/com/rozetka/pages/SearchPage.java
+++ src/main/java/ua/com/rozetka/pages/SearchPage.java
@@ -1,4 +1,43 @@
 package ua.com.rozetka.pages;
 
-public class SearchPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class SearchPage extends GenericPage {
+
+    @FindBy(css = ".search-form__input.ng-pristine")
+    private WebElement searchInput;
+
+    @FindBy(css = ".product-trade .buy-button")
+    private WebElement buyButton;
+
+    @FindBy(css = ".cart-modal__check-button .button-inner")
+    private WebElement orderConfirmButton;
+
+    @FindBy(css = ".catalog-grid > li .goods-tile__title")
+    private List<WebElement> searchItems;
+
+    public SearchPage(WebDriver driver) {
+        super(driver);
+        this.PAGE_TITLE = "Интернет-магазин ROZETKA™: официальный сайт самого популярного онлайн-гипермаркета в Украине";
+        this.PAGE_URL = "https://rozetka.com.ua/";
+    }
+
+    public void setSearchText(String text) {
+        searchElementText(searchInput, text);
+    }
+
+    public void addToCardItem(final String text) {
+        WebElement item = webElements(searchItems).stream()
+                .filter(element -> element.getText().contains(text))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Result with [" + text + "] not found"));
+        scrollToElement(item);
+        clickElement(item);
+        clickElement(buyButton);
+        scrollAndClickElement(orderConfirmButton);
+    }
 }
